8e0d00208048b443c245f13dafd6fe630f99e0b4
rrash <rrash@RASHMI>
2025-11-25 17:45:36 +0530

    pushing through branch ATE

--- src/test/java/Practice/GitPractice_2.java
+++ src/test/java/Practice/GitPractice_2.java
@@ -2,10 +2,14 @@ package Practice;
 
 import org.testng.annotations.Test;
 
+
+
 public class GitPractice_2 {
 	@Test
 	public void m2() {
 		System.out.println("advance selenium");
+		System.out.println("pushing through branch");
+		
 	}
 
 }
